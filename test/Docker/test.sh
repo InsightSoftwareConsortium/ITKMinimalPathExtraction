afd22b64766dca29a299c025e689fcf9a5fcb776
Matt McCormick <matt.mccormick@kitware.com>
2015-10-14 12:45:57 -0400

    ENH: Test the example build.

--- test/Docker/test.sh
+++ test/Docker/test.sh
@@ -16,3 +16,13 @@ cmake \
   -DCMAKE_BUILD_TYPE:STRING=Release \
     /usr/src/ITKMinimalPathExtraction || die "CMake configuration failed"
 ctest -VV -D Experimental || die "ctest failed"
+
+examples_build_dir=/usr/src/ITKMinimalPathExtraction-build/examples
+mkdir -p $examples_build_dir
+cd $examples_build_dir
+cmake \
+  -G Ninja \
+  -DITK_DIR:PATH=/usr/src/ITK-build \
+  -DCMAKE_BUILD_TYPE:STRING=Release \
+    /usr/src/ITKMinimalPathExtraction/examples || die "Example CMake configuration failed"
+ninja || die "examples build failed"
