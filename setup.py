2b5d6f59bef70fe8607c57a235a2d0b03a942a01
Matt McCormick <matt.mccormick@kitware.com>
2019-06-14 15:46:41 -0400

    ENH: CI builds with ITK 5.0.0.post1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 setup(
     name='itk-minimalpathextraction',
-    version='1.0.1',
+    version='1.0.2',
     author='Insight Software Consortium',
     author_email='itk+community@discourse.itk.org',
     packages=['itk'],
@@ -49,6 +49,6 @@ setup(
     keywords='ITK InsightToolkit',
     url=r'https://github.com/InsightSoftwareConsortium/ITKMinimalPathExtraction',
     install_requires=[
-        r'itk>=5.0rc2'
+        r'itk>=5.0.0.post1'
     ]
     )
