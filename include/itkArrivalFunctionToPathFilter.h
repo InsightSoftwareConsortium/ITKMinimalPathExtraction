f82bb1f766124f395d7cf9ee2bf13dcaf2807810
Matt McCormick <matt.mccormick@kitware.com>
2019-11-11 23:54:00 -0500

    STYLE: Use const_cast instead of C cast

--- include/itkArrivalFunctionToPathFilter.h
+++ include/itkArrivalFunctionToPathFilter.h
@@ -63,7 +63,8 @@ public:
   /** Execute */
   void Execute(itk::Object *caller, const itk::EventObject & event) override
     {
-    Execute( (const itk::Object *)caller, event);
+    const itk::Object * constCaller = const_cast< const itk::Object * >( caller );
+    Execute( constCaller, event);
     }
 
   /** Execute */
