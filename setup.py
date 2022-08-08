59931ed3b9bd6eeb716c1673097d999ee8e3e2e5
Stephen R. Aylward <stephen.aylward@kitware.com>
2022-08-08 14:46:03 -0400

    BUG: It is .post2, not -post2, for ITK 5.3 rc4 in setup.py (#94)
    A spelling/punctuation mistake in the setup.py is fixed.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 setup(
     name='itk-minimalpathextraction',
-    version='1.2.1',
+    version='1.2.2',
     author='Insight Software Consortium',
     author_email='itk+community@discourse.itk.org',
     packages=['itk'],
@@ -49,6 +49,6 @@ setup(
     keywords='ITK InsightToolkit',
     url=r'https://github.com/InsightSoftwareConsortium/ITKMinimalPathExtraction',
     install_requires=[
-        r'itk>=5.3rc4-post2'
+        r'itk>=5.3rc4.post2'
     ]
     )
